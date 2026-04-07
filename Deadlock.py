33cef8d1a3ab1154a1affe3362630a31f2023e5b
ShuuDv <eyrzanibg@gmail.com>
2026-04-07 12:11:44 +0700

    Deadlock.py

--- Deadlock.py
+++ Deadlock.py
@@ -1,1 +1,10 @@
+import threading
+import time
 
+#Membuat objek kunci
+kunci1 = threading.Lock()
+kunci2 = threading.Lock()
+
+def thread_a():
+    kunci1.acquire()
+    print("Kunci 1 sudah diambil")
